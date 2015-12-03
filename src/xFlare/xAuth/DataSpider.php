8d48532d07596f355c45e18d73147d4aeefd912b
xFlare <boughousp19@student.riverdell.org>
2015-12-03 08:33:37 -0500

    Spider class, xAuth is now very buggy.

---
+++ src/xFlare/xAuth/DataSpider.php
@@ -0,0 +1,28 @@
+?php
+/*
+                            _     _     
+            /\             | |   | |    
+ __  __    /  \     _   _  | |_  | |__  
+ \ \/ /   / /\ \   | | | | | __| | '_ \ 
+  >  <   / ____ \  | |_| | | |_  | | | |
+ /_/\_\ /_/    \_\  \__,_|  \__| |_| |_|
+                                        
+                                        */
+namespace xFlare\xAuth;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+/*
+- Spiders! What's this class you may ask? Spiders
+= Creep and crawl your player folder to auto delete old player profiles if enabled.
+*/
+class DataSpider implements Listener{
+	public function __construct(Loader $plugin){
+        $this->plugin = $plugin;
+  }
+  public function createSpider(){
+    //
+  }
+}
